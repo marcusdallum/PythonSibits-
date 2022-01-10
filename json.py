b762f763c2aa8ba2915bd48399fb4058b579641d
Marcus <44581072+marcusdallum@users.noreply.github.com>
2022-01-10 15:54:50 -0600

    Update json.py

--- json.py
+++ json.py
@@ -1,3 +1,26 @@
 import json
 import os
 
+filename = "/newjson.json"
+
+#create new json file
+
+newjson = {'Key 1' : {'Key 1.2:'Hello World'}}
+    print(newjson)
+    jsonfile = json.dumps(newjson)
+    f = open(filename, 'a')
+    f.write(jsonfile)
+    f.close
+
+#update a json, load the data to dict, then save over the top
+
+f = open(filename, 'a')
+json_dict = json.load(f)
+f.close()
+# this will update the original data
+json_dict["Key 1"]['Key 1.3'] =  'data'	
+f = open(a , 'w')
+jsonfile = json.dumps(json_dict)
+print(jsonfile)
+f.write(jsonfile)
+f.close()
